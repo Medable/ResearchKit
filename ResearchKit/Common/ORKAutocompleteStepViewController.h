6ff085a9d5cae91d1da8420ec0222daa69d17bdf
Guillermo Biset <billy@medable.com>
2017-03-07 15:27:06 -0300

    ResearchKit 1.2.1.3 with support for Autocomplete Step

---
+++ ResearchKit/Common/ORKAutocompleteStepViewController.h
@@ -0,0 +1,13 @@
+//
+//  ORKAutocompleteStepViewController.h
+//  Pods
+//
+//  Created by Guillermo Biset on 2/22/17.
+//
+//
+
+#import "ORKQuestionStepViewController.h"
+
+@interface ORKAutocompleteStepViewController : ORKQuestionStepViewController
+
+@end
