f30978a5f5c2709caf974ca6c5c61977f6daf2f8
Fer <fer@medable.com>
2018-07-30 14:54:33 -0300

    Better result management. Synchronizing async detection session initialization.

--- ResearchKit/+Medable/ORKMDBarcodeScannerStep/ORKMDBarcodeScannerStepViewController.m
+++ ResearchKit/+Medable/ORKMDBarcodeScannerStep/ORKMDBarcodeScannerStepViewController.m
@@ -17,13 +17,14 @@
 #import "ORKStepHeaderView_Internal.h"
 #import "ORKStepViewController_Internal.h"
 
-@interface ORKMDBarcodeScannerStepViewController() <ORKMDBarcodeScannerViewDelegate>
+@interface ORKMDBarcodeScannerStepViewController () <ORKMDBarcodeScannerViewDelegate>
 
-@property (nonatomic, strong) ORKStepResult *result;
-@property (nonatomic, strong) ORKMDBarcodeScanner *scanner;
+@property (nonatomic, strong) ORKInstructionStepView *instructionStepView;
+@property (nonatomic, strong) ORKMDBarcodeScanner *scannerView;
 
-@property (nonatomic, readonly) ORKInstructionStepView *stepView;
 @property (nonatomic, readonly) ORKNavigationContainerView *navigation;
+
+@property (nonatomic, copy) NSString *scannerOutput;
 
 @end
 
@@ -37,8 +38,7 @@
     
     if (self)
     {
-        NSParameterAssert([step isKindOfClass:
-                           [ORKMDBarcodeScannerStep class]]);
+        NSParameterAssert([step isKindOfClass:[ORKMDBarcodeScannerStep class]]);
     }
     
     return self;
@@ -46,117 +46,59 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    
     [self stepDidChange];
-
-    [self.view addSubview:self.stepView];
     
-    // barcodeScanner must be on top
-    [self.view addSubview:self.scanner];
-
-    if (self.scanner.isConfigured)
-    {
-        self.stepView.headerView.captionLabel.text = @"Scan was Successful";
-        self.stepView.headerView.instructionLabel.text = @"you should be proud!";
-    }
-    else
-    {
-        self.stepView.headerView.captionLabel.text = @"Unable to Scan";
-        
-        [self.stepView.headerView.instructionLabel setText:
-         @"There was a problem using the camera,\nor this device does not support"
-         " scanning. Please try updating this device to use the latest version of iOS."];
-    }
+    self.instructionStepView = [[ORKInstructionStepView alloc] initWithFrame:self.view.bounds];
+    self.instructionStepView.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
+    [self.view addSubview:self.instructionStepView];
+    
+    self.scannerView = [[ORKMDBarcodeScanner alloc] initWithFrame:self.view.bounds];
+    self.scannerView.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
+    self.scannerView.delegate = self;
+    [self.view addSubview:self.scannerView];
+    
+    [super viewDidLoad];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    [self.scanner startScanning];
+    
+    [self.scannerView startScanning];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
     [super viewWillDisappear:animated];
-    [self.scanner stopScanning];
+    
+    [self.scannerView stopScanning];
 }
+
 
 #pragma mark -
 
-@synthesize result = _result;
-
-- (ORKStepResult*)result
-{
-    return _result ?: super.result;
-}
-
-@synthesize scanner = _scanner;
-
-- (ORKMDBarcodeScanner*)scanner
-{
-    if (_scanner != nil) return _scanner;
-    
-    // use "self." so setter is called
-    self.scanner = [[ORKMDBarcodeScanner alloc]
-                    initWithFrame:self.view.bounds];
-    
-    _scanner.autoresizingMask = (UIViewAutoresizingFlexibleWidth |
-                                 UIViewAutoresizingFlexibleHeight);
-    return _scanner;
-}
-
-- (void)setScanner:(ORKMDBarcodeScanner*)scanner
-{
-    if (_scanner != scanner)
-    {
-        _scanner.delegate = nil;
-        [_scanner removeFromSuperview];
-        
-        _scanner = scanner;
-        _scanner.delegate = self;
-    }
-}
-
-@synthesize stepView = _stepView;
-
-- (ORKInstructionStepView *)stepView
-{
-    if (_stepView != nil) return _stepView;
-    
-    _stepView = [[ORKInstructionStepView alloc]
-                 initWithFrame:self.view.bounds];
-    
-    _stepView.autoresizingMask = (UIViewAutoresizingFlexibleWidth |
-                                  UIViewAutoresizingFlexibleHeight);
-
-    return _stepView;
-}
-
 - (ORKNavigationContainerView*)navigation
 {
-    return self.stepView.continueSkipContainer;
+    return self.instructionStepView.continueSkipContainer;
 }
 
-- (void)saveAnswer:(NSString*)answer
+- (ORKStepResult *)result
 {
-    _result = super.result;
+    ORKStepResult *stepResult = [super result];
+    NSDate *now = stepResult.endDate;
+
+    NSMutableArray *results = [NSMutableArray arrayWithArray:stepResult.results];
     
-    if (answer)
-    {
-        ORKTextQuestionResult *result = [[ORKTextQuestionResult alloc]
-                                         initWithIdentifier:self.step.identifier];
-        result.textAnswer = answer;
-        result.questionType = ORKQuestionTypeText;
-
-        result.endDate = _result.endDate;
-        result.startDate = _result.startDate;
-
-        _result.results = @[result];
-       
-        [self notifyDelegateOnResultChange];
-        self.navigation.continueEnabled = YES;
-    }
+    ORKTextQuestionResult *result = [[ORKTextQuestionResult alloc] initWithIdentifier:self.step.identifier];
+    result.textAnswer = self.scannerOutput;
+    result.questionType = ORKQuestionTypeText;
+    result.endDate = now;
+    result.startDate = stepResult.startDate;
+    
+    [results addObject:result];
+    stepResult.results = [results copy];
+    
+    return stepResult;
 }
 
 - (void)stepDidChange
@@ -166,15 +108,16 @@
     if (self.step && self.isViewLoaded)
     {
         self.navigation.continueEnabled = YES;
-        self.stepView.continueSkipContainer.hidden = NO;
+        self.instructionStepView.continueSkipContainer.hidden = NO;
         self.navigation.continueButtonItem = self.continueButtonItem;
-        self.stepView.headerView.learnMoreButtonItem = self.learnMoreButtonItem;
+        self.instructionStepView.headerView.learnMoreButtonItem = self.learnMoreButtonItem;
     }
 }
 
 - (void)setSkipButtonItem:(UIBarButtonItem *)skipButtonItem
 {
     [super setSkipButtonItem:skipButtonItem];
+    
     self.navigation.skipButtonItem = skipButtonItem;
     self.navigation.skipEnabled = self.step.isOptional;
 }
@@ -182,21 +125,56 @@
 - (void)setContinueButtonItem:(UIBarButtonItem *)continueButtonItem
 {
     [super setContinueButtonItem:continueButtonItem];
+    
     self.navigation.continueButtonItem = continueButtonItem;
 }
 
 - (void)setLearnMoreButtonItem:(UIBarButtonItem *)learnMoreButtonItem
 {
     [super setLearnMoreButtonItem:learnMoreButtonItem];
-    self.stepView.headerView.learnMoreButtonItem = learnMoreButtonItem;
+    
+    self.instructionStepView.headerView.learnMoreButtonItem = learnMoreButtonItem;
 }
 
-#pragma mark -
 
-- (void)didProduceMetadataOutput:(AVMetadataMachineReadableCodeObjectArray*) output
+#pragma mark - ORKMDBarcodeScannerViewDelegate
+
+- (void)didFinishConfiguration
 {
-    self.scanner = nil; // tear down the scanner
-    [self saveAnswer:output.firstObject.stringValue];
+    [[NSOperationQueue mainQueue] addOperationWithBlock:^
+     {
+         if (self.scannerView.isConfigured)
+         {
+             self.instructionStepView.headerView.captionLabel.text = @"Scan was Successful";
+             self.instructionStepView.headerView.instructionLabel.text = @"you should be proud!";
+         }
+         else
+         {
+             self.instructionStepView.headerView.captionLabel.text = @"Unable to Scan";
+             
+             [self.instructionStepView.headerView.instructionLabel setText:
+              @"There was a problem using the camera,\nor this device does not support"
+              " scanning. Please try updating this device to use the latest version of iOS."];
+         }
+     }];
+}
+
+- (void)didProduceMetadataOutput:(AVMetadataMachineReadableCodeObjectArray *)output
+{
+    NSString *result = output.firstObject.stringValue;
+    self.scannerOutput = result;
+    
+    if (result.length)
+    {
+        [self.scannerView removeFromSuperview];
+        self.scannerView = nil; // tear down the scanner
+        
+        [self notifyDelegateOnResultChange];
+        self.navigation.continueEnabled = YES;
+
+        [self goForward];
+    }
+    
 }
 
 @end
