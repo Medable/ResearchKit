af6ab8b6123ef53052ab05b4989b0e273433318a
stefan-bogdanovic-medable <58979111+stefan-bogdanovic-medable@users.noreply.github.com>
2021-01-27 11:31:46 +0100

    Revert "AXONIOS-182 canceling image capture was removing previous image data"

--- ResearchKit/Common/ORKImageCaptureStepViewController.m
+++ ResearchKit/Common/ORKImageCaptureStepViewController.m
@@ -299,13 +299,11 @@
 }
 
 - (void)setCapturedImageData:(NSData *)capturedImageData {
-    BOOL shouldRemoveFile = capturedImageData != nil;
-    
     _capturedImageData = capturedImageData;
     _imageCaptureView.capturedImage = capturedImageData ? [UIImage imageWithData:capturedImageData] : nil;
     
     // Remove the old file, if it exists, now that new data was acquired or reset
-    if (_fileURL && shouldRemoveFile) {
+    if (_fileURL) {
         [[NSFileManager defaultManager] removeItemAtURL:_fileURL error:nil];
         // Force the file to be rewritten the next time the result is requested
         _fileURL = nil;
